84298729384e69ed3d3298e24822cac9eb5431e1
Akash <122150350+akashm6@users.noreply.github.com>
2024-10-21 02:31:59 -0500

    google analytics integration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const apiKey = process.env.API_KEY;
+const storageBucket = process.env.STORAGE_BUCKET;
+const msId = process.env.MS_ID;
+const appId = process.env.APP_ID;
+const measurementId = process.env.MEASUREMENT_ID;
+
+const firebaseConfig = {
+  apiKey: {apiKey},
+  authDomain: "akashmohan.me",
+  projectId: "akashmohanportfolio",
+  storageBucket: {storageBucket},
+  messagingSenderId: {msID},
+  appId: {appId},
+  measurementId: {measurementId}
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +33,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
